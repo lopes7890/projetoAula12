36fe036a19e540396378b30e27e4d9fd5feb0646
estudos etec <estudos etec@ALLISONLOPES08>
2024-10-29 14:30:59 -0300

    Adicionar arquivos de projeto.

---
+++ projetoAula12/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace projetoAula12
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
